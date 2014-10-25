57be10edd5473f97360df2f59a8292dac8c4d1c8
moznion <moznion@gmail.com>
2014-10-26 00:25:37 +0900

    Take rows from head

---
+++ 1/5_head.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+fh = open('./resource/address.txt', encoding='utf-8')
+rows = fh.readlines()
+
+line_num = int(sys.argv[1])
+
+for row in rows[0:line_num]:
+    print(row.rstrip('\r\n'))
